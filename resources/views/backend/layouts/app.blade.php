cfd24d8c84f142f423d16daf5d0ad3213f6085ba
= <=>
2021-02-04 10:14:48 +0300

    fix sidebar

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ appName() }} | @yield('title')</title>
     <meta name="description" content="@yield('meta_description', appName())">
-    <meta name="author" content="@yield('meta_author', 'John Ruita')">
+    <meta name="author" content="@yield('meta_author', 'Anthony Rappa')">
     @yield('meta')
 
     @stack('before-styles')
@@ -14,44 +14,42 @@
     <livewire:styles />
     @stack('after-styles')
 </head>
-<body class="hold-transition sidebar-mini">
-    @include('backend.includes.sidebar')
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
     @include('backend.includes.header')
-
-    <div class="wrapper">
-        <div class="content-wrapper">
-            @include('includes.partials.read-only')
-            @include('includes.partials.logged-in-as')
-            @include('includes.partials.announcements')
-            <div class="content-header">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <h1 class="m-0 text-dark">@yield('title')</h1>
-                        </div>
-                        <div class="col-sm-6">
-                            @include('backend.includes.partials.breadcrumbs')
-                        </div>
+    @include('backend.includes.sidebar')
+    <div class="content-wrapper">
+        @include('includes.partials.read-only')
+        @include('includes.partials.logged-in-as')
+        @include('includes.partials.announcements')
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <h1 class="m-0 text-dark">@yield('title')</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        @include('backend.includes.partials.breadcrumbs')
                     </div>
                 </div>
             </div>
-            <main class="content">
-                <div class="container-fluid">
-                    <div class="fade-in">
-                        @include('includes.partials.messages')
-                        @yield('content')
-                    </div>
-                </div>
-            </main>
         </div>
-        @include('backend.includes.footer')
+        <main class="content">
+            <div class="container-fluid">
+                <div class="fade-in">
+                    @include('includes.partials.messages')
+                    @yield('content')
+                </div>
+            </div>
+        </main>
     </div>
-
-    @stack('before-scripts')
-    <script src="{{ mix('js/manifest.js') }}"></script>
-    <script src="{{ mix('js/vendor.js') }}"></script>
-    <script src="{{ mix('js/backend.js') }}"></script>
-    <livewire:scripts />
-    @stack('after-scripts')
+    @include('backend.includes.footer')
+</div>
+@stack('before-scripts')
+<script src="{{ mix('js/manifest.js') }}"></script>
+<script src="{{ mix('js/vendor.js') }}"></script>
+<script src="{{ mix('js/backend.js') }}"></script>
+<livewire:scripts />
+@stack('after-scripts')
 </body>
 </html>
--- resources/views/components/utils/link.blade.php
+++ resources/views/components/utils/link.blade.php
@@ -1,13 +1,17 @@
-@props(['active' => '', 'text' => '', 'hide' => false, 'icon' => false, 'permission' => false])
+@props(['active' => '', 'text' => '', 'hide' => false, 'icon' => false, 'permission' => false, 'sidebar' => false])
 
 @if ($permission)
     @if ($logged_in_user->can($permission))
         @if (!$hide)
-            <a {{ $attributes->merge(['href' => '#', 'class' => $active]) }}>@if ($icon)<i class="{{ $icon }}"></i> @endif<p class="d-inline">{{ strlen($text) ? $text : $slot }}</p></a>
+            <a {{ $attributes->merge(['href' => '#', 'class' => $active]) }}>@if ($icon)<i class="{{ $icon }}"></i> @endif{{ strlen($text) ? $text : $slot }}</a>
         @endif
+    @endif
+@elseif($sidebar)
+    @if (!$hide)
+        <a {{ $attributes->merge(['href' => '#', 'class' => $active]) }}>@if ($icon)<i class="{{ $icon }}"></i> @endif <p>{{ strlen($text) ? $text : $slot }}</p> </a>
     @endif
 @else
     @if (!$hide)
-        <a {{ $attributes->merge(['href' => '#', 'class' => $active]) }}>@if ($icon)<i class="{{ $icon }}"></i> @endif<p class="d-inline">{{ strlen($text) ? $text : $slot }}</p></a>
+        <a {{ $attributes->merge(['href' => '#', 'class' => $active]) }}>@if ($icon)<i class="{{ $icon }}"></i> @endif{{ strlen($text) ? $text : $slot }}</a>
     @endif
 @endif
